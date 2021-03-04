506dbcfb6945f281346896784017d9d93c514190
sambitc <sambit.gudu@yahoo.co.in>
2021-03-04 23:57:08 +0530

    updated

--- pipeline1/pipeline-run.sh
+++ pipeline1/pipeline-run.sh
@@ -7,6 +7,7 @@ tkn pipeline start pl-gitpull-mavenbuild1 \
 	-w name=ws,claimName=workspace-pvc \
 	-p git-url=https://github.com/sambitc/hello-docker.git \
 	-p git-revision=6aacda29659f1bef32fe81a1cdf8782be6e597f7 \
-	-p image=docker.io/sambitc/hello-docker:tkn
+	-p image=docker.io/sambitc/hello-docker:tkn \
+	--serviceaccount=pl-sa
 	
 
